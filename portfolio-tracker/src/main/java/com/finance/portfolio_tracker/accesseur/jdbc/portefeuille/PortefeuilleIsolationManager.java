2cc04221cec7271335f288dfeb86730a133068d8
Yev <yevh.bondarenko@gmail.com>
2026-03-08 20:53:52 +0100

    added missing methods to the isolation manager

--- portfolio-tracker/src/main/java/com/finance/portfolio_tracker/accesseur/jdbc/portefeuille/PortefeuilleIsolationManager.java
+++ portfolio-tracker/src/main/java/com/finance/portfolio_tracker/accesseur/jdbc/portefeuille/PortefeuilleIsolationManager.java
@@ -1,6 +1,8 @@
 package com.finance.portfolio_tracker.accesseur.jdbc.portefeuille;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
@@ -20,14 +22,27 @@ public class PortefeuilleIsolationManager {
     }
 
     public PortefeuilleInterne save(PortefeuilleInterne portefeuille) {
+        //converts interne model to accesseur
         var accesseur = mappeur.map(portefeuille);
+        //saves to database with assigned id (if new)
         var saved = repository.save(accesseur);
+        // converts saved back to interne with id
         var interne = mappeur.map(saved);
         return interne;
     }
 
     public Optional<PortefeuilleInterne> findById(Long id) {
         return repository.findById(id).map(mappeur::map);
+    }
+
+    public List<PortefeuilleInterne> findAll() {
+        return repository.findAll().stream()
+        .map(mappeur::map)
+        .collect(Collectors.toList());
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
     }
     
 }
